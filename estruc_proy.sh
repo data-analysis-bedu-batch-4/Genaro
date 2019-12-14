ef893205512455b94b83b55940d9f066b9629025
g-galindo <genaro.galindo@gmail.com>
2019-12-14 09:59:48 -0600

    primer proyecto

---
+++ estruc_proy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir Proy_MedioAmbiente
+cd Proy_MedioAmbiente
+
+mkdir Datos
+mkdir Documentos
+mkdir Resultados
+mkdir Presentacion
+
+
+
+echo la estructura del proyecto ha sido generada
